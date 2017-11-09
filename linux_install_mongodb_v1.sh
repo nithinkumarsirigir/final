7942e096d65f9eaaa1520d28571e6f8a5a245364
nithinkumarsirigir <33519642+nithinkumarsirigir@users.noreply.github.com>
2017-11-09 21:12:26 +0530

    Update linux_install_mongodb_v1.sh

--- linux_install_mongodb_v1.sh
+++ linux_install_mongodb_v1.sh
@@ -1,42 +1,72 @@
 #!/bin/bash
 
+
+
 echo "Installing MongoDb"
 
+
+
 isApt=`command -v apt-get`
+
 isYum=`command -v yum`
+
 isZypper=`command -v zypper`
-# Some of the previous commands will fail with an exit code other than zero (intentionally),
+
+
+# Some of the previous commands will fail with an exit code other than zero (intentionally), 
+
 # so we do not set error handling to stop (set e) until after they've run
+
 set -e
 
+
+
 if [ -n "$isApt" ] ; then
+
     echo "Using APT package manager"
 
+    
+
     apt-get -y update
+
     apt-get -y install mongodb
 
-elif [ -n "$iszypper" ] ; then
-echo "Using zypper package manager"
-    zypper -y update
-    zypper -y install mangodb
-elif [ -n "$isYum" ] ; then
-    echo "Using YUM package manager"
+elif [ -n "$isZypper" ] ; then
 
-    yum clean all
-    yum -y update
+    echo "Using zypper package manager"
+
+
+
+    zypper clean all
+
+    zypper -y update
+
+    
 
     releasever=7
+
     cat >/etc/yum.repos.d/mongodb-org-3.0.repo <<-EOF
+
 [mongodb-org-3.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
+
+name=Mysql Repository
+
+baseurl=http://download.opensuse.org/repositories/devel:languages:python/SLE_12_SP2/devel:languages:python.repo
+
 gpgcheck=0
+
 enabled=1
+
 EOF
 
-    yum install -y mongodb-org
+    
 
-    service mongod start
-    sudo service mongod restart
-    chkconfig mongod on
+    zypper install -y zypper
+
+    
+
+    service mysql start
+
+    chkconfig mysql on
+
 fi
